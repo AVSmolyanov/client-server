0c6908de5264cab1e4ecaaaa3dd6fcec74cbc848
AVSmolyanov <ASmolyanov@mail.ru>
2022-05-26 14:22:00 +1000

    2-nd

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -9,14 +9,9 @@ import java.net.Socket;
 
 public class Server {
 
-    private static int port;
-    private static ServerSocket serverSocket;
-    private static Socket clientSocket;
-    private static PrintWriter out;
-    private static BufferedReader in;
     private static String name;
 
-    static String ask(String question, String isFinish) throws IOException {
+    static String ask(BufferedReader in, PrintWriter out, String question, String isFinish) throws IOException {
         String answer = "";
         out.println(String.format(question));
         out.println(String.format(isFinish));
@@ -27,36 +22,46 @@ public class Server {
     }
 
     public static void main(String[] args) throws IOException {
-        port = 8083;
-        serverSocket = new ServerSocket(port);
-        System.out.println("Server started.");
-        while (true) {
-            System.out.println("Awaiting the connection...");
-            clientSocket = serverSocket.accept(); // ждем подключения
-            out = new PrintWriter(clientSocket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        int port = 8083;
+        try (ServerSocket serverSocket = new ServerSocket(port);) {
+            System.out.println("Server started.");
+            while (true) {
+                System.out.println("Awaiting the connection...");
+                try (
+                        Socket clientSocket = serverSocket.accept(); // ждем подключения
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
 
-            System.out.println(String.format("New connection accepted by port %d", clientSocket.getPort()));
-            String answer = in.readLine();
+                    System.out.println(String.format("New connection accepted by port %d", clientSocket.getPort()));
+                    String answer = in.readLine();
 
-            System.out.println("CLIENT: " + answer);
-            name = ask("Write your name", "No");
-            String data;
-            do {
-                data = ask("Are you child? (yes/no)", "No");
-            } while (!("yes".equals(data) || "no".equals(data)));
+                    System.out.println("CLIENT: " + answer);
+                    name = ask(in, out, "Write your name", "No");
+                    String data;
+                    do {
+                        data = ask(in, out, "Are you child? (yes/no)", "No");
+                        if (data == null) {
+                            System.out.println("Incorrect answer from Client or it's session terminated.");
+                            break;
+                        }
+                    } while (!("yes".equals(data) || "no".equals(data)));
 
-
-            switch (data) {
-                case "yes":
-                    ask("Welcome to the kids area, " + name + "! Let's play!", "Yes");
-                    break;
-                default:
-                    ask("Welcome to the adult zone, " + name + "! Have a good rest, or a good working day!", "Yes");
-                    break;
+                    switch (data) {
+                        case "yes":
+                            ask(in, out, "Welcome to the kids area, " + name + "! Let's play!", "Yes");
+                            break;
+                        default:
+                            ask(in, out, "Welcome to the adult zone, " + name + "! Have a good rest, or a good working day!", "Yes");
+                            break;
+                    }
+                    System.out.println("Session finished. Connection closed");
+                    clientSocket.close();
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
             }
-            System.out.println("Session finished. Connection closed");
-            clientSocket.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
